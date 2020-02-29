e8419b8667512fef986516b8db91a8f3156a66ad
manjunatha96 <58422091+manjunatha96@users.noreply.github.com>
2020-02-29 14:24:00 +0530

    second commit

--- Controller/BigdataController.js
+++ Controller/BigdataController.js
@@ -1,81 +1,88 @@
 const express=require('express');
 const router=express.Router();
-const {BigData} =require('../Tables/Bigdata')
-
-router.get('/get',async(req,res)=>{
-    await res.send('getting data')
-})
 
 router.post('/postMul', async(req,res)=>{
-    
-    console.log(req.body);
-    if(req.body.op=='equal'){
-        console.log('=');
-    }
-    console.log(req.body.rhs)
     op3=req.body.lhs
-    op2=op3.rhs
-
-    switch (op2.op) {
-        case "multiply":
-            oprt=op2.lhs*op2.rhs
-            opr=`(${op2.lhs} * ${op2.rhs})`
-            console.log(oprt);
-            break;
-        case "add":
-            oprt=op2.lhs+op2.rhs
-            opr=`(${op2.lhs} + ${op2.rhs})`
-            console.log(oprt);
-            break;        
-        case "subtract":
-            oprt=op2.lhs-op2.rhs
-            opr=`(${op2.lhs} - ${op2.rhs})`
-            console.log(oprt);
-            break;
-        case "divide":
-            oprt=op2.lhs/op2.rhs
-            opr=`(${op2.lhs} / ${op2.rhs})`
-            console.log(oprt);
-            break;
-        default:
-            console.log('not valid oprator');
-    }
-   
     op1=req.body.lhs
-    console.log(op1);
-    
+    op2=op3.rhs
 
     switch (op1.op) {
         case "multiply":
+            oprt=op2.lhs*op2.rhs
+            oprt11=req.body.rhs*op1.lhs
+            oprt12=req.body.rhs/op1.lhs
+            opr=`${op1.lhs} *`
+            opr11=`(${req.body.rhs} / ${op1.lhs})`
+            console.log('-',oprt);            
+            break;
+        case "add":
+            oprt=op2.lhs+op2.rhs
+            oprt11=req.body.rhs+op1.lhs
+            oprt12=req.body.rhs-op1.lhs
+            opr=`${op1.lhs} +`
+            opr11=`(${req.body.rhs} - ${op1.lhs})`
+        
+            break;        
+        case "subtract":
+            oprt=op2.lhs-op2.rhs
+            oprt11=req.body.rhs-op1.lhs
+            oprt12=req.body.rhs+op1.lhs
+            opr=`${op1.lhs} -`
+            opr11=`(${req.body.rhs} + ${op1.lhs})`
+        
+            break;
+        case "divide":
+            oprt=op2.lhs/op2.rhs
+            oprt11=req.body.rhs/op1.lhs
+            oprt12=req.body.rhs*op1.lhs
+            opr=`${op1.lhs} /`
+            opr11=`(${req.body.rhs} * ${op1.lhs})`
+            console.log(oprt12);            
+            break;
+        default:
+            console.log('Not valid oprator');
+    }
+       
+    switch (op2.op) {
+        case "multiply":
             oprt1=op1.lhs*oprt
-            opr1=`${op1.lhs} * ${opr}`
-            console.log(opr1);
-            console.log(oprt1==req.body.rhs);
-
+            oprt21=oprt11*op2.rhs
+            oprt22=oprt12/op2.rhs
+            opr1=`${opr}( ${op2.lhs} * ${op2.rhs})= ${req.body.rhs}`
+            opr21=`${op2.lhs} = ${opr11} / ${op2.rhs}`
+            opr31=`${op2.lhs} = ${oprt22}`
             break;
         case "add":
             oprt1=op1.lhs+oprt
-            opr1=`${op1.lhs} + ${opr}`
-            console.log(opr1);
-            console.log(oprt1==req.body.rhs);
+            oprt21=oprt11+op2.rhs
+            oprt22=oprt12-op2.rhs
+            opr1=`${opr}( ${op2.lhs} + ${op2.rhs})= ${req.body.rhs}`
+            opr21=`${op2.lhs} = ${opr11} - ${op2.rhs}`
+            opr31=`${op2.lhs} = ${oprt22}`
             break;        
         case "subtract":
             oprt1=op1.lhs-oprt
-            opr1=`${op1.lhs} - ${opr}`
-            console.log(opr1);
-            console.log(oprt1==req.body.rhs);
+            oprt21=oprt11-op2.rhs
+            oprt22=oprt12+op2.rhs
+            opr1=`${opr}( ${op2.lhs} - ${op2.rhs})= ${req.body.rhs}`
+            opr21=`${op2.lhs} = ${opr11} + ${op2.rhs}`
+            opr31=`${op2.lhs} = ${oprt22}`
             break;
         case "divide":
             oprt1=op1.lhs/oprt
-            opr1=`${op1.lhs} / ${opr}`
-            console.log(opr1);
-            console.log(oprt1==req.body.rhs);
+            oprt21=oprt11/op2.rhs
+            oprt22=oprt12*op2.rhs
+            opr1=`${opr}( ${op2.lhs} / ${op2.rhs})= ${req.body.rhs}`
+            opr21=`${op2.lhs} = ${opr11} * ${op2.rhs}`
+            opr31=`${op2.lhs} = ${oprt22}`
             break;
         default:
-            console.log('not valid oprator');
+            console.log('Not valid oprator');
     }
     
-    res.send('result')
+    res.send(`Problem 1) ${opr1}
+            problem 2) ${opr21}
+            solution 3) ${opr31}`)
     
 })
 
